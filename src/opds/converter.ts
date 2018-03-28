8b6cfcaa4d83af04f37396a598910bf7ff5a00a2
danielweck <daniel.weck@gmail.com>
2018-03-28 15:31:22 +0100

    added license header in TypeScript source, see https://github.com/edrlab/readium-desktop/issues/105

--- src/opds/converter.ts
+++ src/opds/converter.ts
@@ -1,3 +1,10 @@
+// ==LICENSE-BEGIN==
+// Copyright 2017 European Digital Reading Lab. All rights reserved.
+// Licensed to the Readium Foundation under one or more contributor license agreements.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file exposed on Github (readium) in the project repository.
+// ==LICENSE-END==
+
 import { OPDS } from "./opds1/opds";
 import { OPDSFeed } from "./opds2/opds2";
 import { OPDSBelongsTo } from "./opds2/opds2-belongsTo";
