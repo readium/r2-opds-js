900d7e98db6bc410737ed74afd50751649a4c099
Daniel Weck <daniel.weck@gmail.com>
2018-11-14 17:22:38 +0000

    OPDS 1 - 2 converter now exposes function to convert single Entry

--- src/opds/converter.ts
+++ src/opds/converter.ts
@@ -7,8 +7,8 @@
 
 import { BelongsTo } from "@r2-shared-js/models/metadata-belongsto";
 import { Subject } from "@r2-shared-js/models/metadata-subject";
-
 import { OPDS } from "./opds1/opds";
+import { Entry } from "./opds1/opds-entry";
 import { OPDSFeed } from "./opds2/opds2";
 import { OPDSCollection } from "./opds2/opds2-collection";
 import { OPDSContributor } from "./opds2/opds2-contributor";
@@ -19,6 +19,196 @@ import { OPDSPrice } from "./opds2/opds2-price";
 import { OPDSProperties } from "./opds2/opds2-properties";
 import { OPDSPublication } from "./opds2/opds2-publication";
 import { OPDSPublicationMetadata } from "./opds2/opds2-publicationMetadata";
+
+export function convertOpds1ToOpds2_EntryToPublication(entry: Entry): OPDSPublication {
+
+    const p = new OPDSPublication();
+    p.Metadata = new OPDSPublicationMetadata();
+    p.Metadata.Title = entry.Title;
+    if (entry.DcIdentifier) {
+        p.Metadata.Identifier = entry.DcIdentifier;
+    } else {
+        p.Metadata.Identifier = entry.Id;
+    }
+    if (entry.DcLanguage) {
+        p.Metadata.Language = [entry.DcLanguage];
+    }
+    p.Metadata.Modified = entry.Updated;
+    p.Metadata.PublicationDate = entry.Published;
+    p.Metadata.Rights = entry.DcRights;
+    if (entry.Series) {
+        entry.Series.forEach((s) => {
+            const coll = new OPDSCollection();
+            coll.Name = s.Name;
+            coll.Position = s.Position;
+            const link = new OPDSLink();
+            link.Href = s.Url;
+            coll.Links = [];
+            coll.Links.push(link);
+
+            if (!p.Metadata.BelongsTo) {
+                p.Metadata.BelongsTo = new BelongsTo();
+            }
+            if (!p.Metadata.BelongsTo.Series) {
+                p.Metadata.BelongsTo.Series = [];
+            }
+            p.Metadata.BelongsTo.Series.push(coll);
+        });
+    }
+    if (entry.DcPublisher) {
+        const c = new OPDSContributor();
+        c.Name = entry.DcPublisher;
+        if (!p.Metadata.Publisher) {
+            p.Metadata.Publisher = [];
+        }
+        p.Metadata.Publisher.push(c);
+    }
+
+    if (entry.Categories) {
+        entry.Categories.forEach((cat) => {
+            const subj = new Subject();
+            subj.Code = cat.Term;
+            subj.Name = cat.Label;
+            subj.Scheme = cat.Scheme;
+            if (!p.Metadata.Subject) {
+                p.Metadata.Subject = [];
+            }
+            p.Metadata.Subject.push(subj);
+        });
+    }
+    if (entry.Authors) {
+        entry.Authors.forEach((aut) => {
+
+            const cont = new OPDSContributor();
+            cont.Name = aut.Name;
+            cont.Identifier = aut.Uri;
+            if (!p.Metadata.Author) {
+                p.Metadata.Author = [];
+            }
+            p.Metadata.Author.push(cont);
+        });
+    }
+
+    if (entry.Summary) {
+        p.Metadata.Description = ((entry.SummaryType === "text/html" || entry.SummaryType === "html") ?
+            entry.Summary.replace(/ xmlns="[^"]+"/g, "") : // xmlns="http://www.w3.org/2005/Atom"
+            entry.Summary);
+    }
+
+    if (entry.Content) {
+        const txt = ((entry.ContentType === "text/html" || entry.ContentType === "html") ?
+            entry.Content.replace(/ xmlns="[^"]+"/g, "") : // xmlns="http://www.w3.org/2005/Atom"
+            entry.Content);
+
+        if (p.Metadata.Description) {
+            p.Metadata.Description += "\n\n";
+            p.Metadata.Description += txt;
+        } else {
+            p.Metadata.Description = txt;
+        }
+    }
+
+    if (entry.Links) {
+        entry.Links.forEach((link) => {
+            const l = new OPDSLink();
+            l.Href = link.Href;
+            l.TypeLink = link.Type;
+            l.AddRel(link.Rel);
+            l.Title = link.Title;
+
+            if (link.OpdsIndirectAcquisitions && link.OpdsIndirectAcquisitions.length) {
+                if (!l.Properties) {
+                    l.Properties = new OPDSProperties();
+                }
+
+                link.OpdsIndirectAcquisitions.forEach((ia) => {
+                    const ind = new OPDSIndirectAcquisition();
+                    ind.TypeAcquisition = ia.OpdsIndirectAcquisitionType;
+                    if (ia.OpdsIndirectAcquisitions && ia.OpdsIndirectAcquisitions.length) {
+                        ia.OpdsIndirectAcquisitions.forEach((iac) => {
+
+                            const cia = new OPDSIndirectAcquisition();
+                            cia.TypeAcquisition = iac.OpdsIndirectAcquisitionType;
+                            if (!ind.Children) {
+                                ind.Children = [];
+                            }
+                            ind.Children.push(cia);
+                        });
+                    }
+                    if (!l.Properties.IndirectAcquisitions) {
+                        l.Properties.IndirectAcquisitions = [];
+                    }
+                    l.Properties.IndirectAcquisitions.push(ind);
+                });
+            }
+
+            if (link.OpdsPrice && link.OpdsPriceCurrencyCode) {
+                if (!l.Properties) {
+                    l.Properties = new OPDSProperties();
+                }
+                l.Properties.Price = new OPDSPrice();
+                l.Properties.Price.Currency = link.OpdsPriceCurrencyCode;
+                l.Properties.Price.Value = link.OpdsPrice;
+            }
+
+            if (link.HasRel("collection") || link.HasRel("http://opds-spec.org/group")) {
+                // NOOP
+            } else if (link.HasRel("http://opds-spec.org/image") ||
+                link.HasRel("http://opds-spec.org/image/thumbnail")) {
+                if (!p.Images) {
+                    p.Images = [];
+                }
+                p.Images.push(l);
+            } else {
+                if (!p.Links) {
+                    p.Links = [];
+                }
+                p.Links.push(l);
+            }
+        });
+    }
+
+    return p;
+}
+export function convertOpds1ToOpds2_EntryToLink(entry: Entry): OPDSLink {
+
+    const linkNav = new OPDSLink();
+    linkNav.Title = entry.Title;
+
+    if (entry.Summary) {
+        const txt = ((entry.SummaryType === "text/html" || entry.SummaryType === "html") ?
+            entry.Summary.replace(/ xmlns="[^"]+"/g, "") : // xmlns="http://www.w3.org/2005/Atom"
+            entry.Summary);
+
+        if (linkNav.Title) {
+            linkNav.Title += "\n\n";
+            linkNav.Title += txt;
+        } else {
+            linkNav.Title = txt;
+        }
+    }
+
+    if (entry.Content) {
+        const txt = ((entry.ContentType === "text/html" || entry.ContentType === "html") ?
+            entry.Content.replace(/ xmlns="[^"]+"/g, "") : // xmlns="http://www.w3.org/2005/Atom"
+            entry.Content);
+
+        if (linkNav.Title) {
+            linkNav.Title += "\n\n";
+            linkNav.Title += txt;
+        } else {
+            linkNav.Title = txt;
+        }
+    }
+
+    if (entry.Links && entry.Links[0]) {
+        linkNav.AddRel(entry.Links[0].Rel);
+        linkNav.TypeLink = entry.Links[0].Type;
+        linkNav.Href = entry.Links[0].Href;
+    }
+
+    return linkNav;
+}
 
 // https://github.com/opds-community/opds-revision
 export function convertOpds1ToOpds2(feed: OPDS): OPDSFeed {
@@ -67,152 +257,7 @@ export function convertOpds1ToOpds2(feed: OPDS): OPDSFeed {
             }
 
             if (!isAnNavigation) {
-                const p = new OPDSPublication();
-                p.Metadata = new OPDSPublicationMetadata();
-                p.Metadata.Title = entry.Title;
-                if (entry.DcIdentifier) {
-                    p.Metadata.Identifier = entry.DcIdentifier;
-                } else {
-                    p.Metadata.Identifier = entry.Id;
-                }
-                if (entry.DcLanguage) {
-                    p.Metadata.Language = [entry.DcLanguage];
-                }
-                p.Metadata.Modified = entry.Updated;
-                p.Metadata.PublicationDate = entry.Published;
-                p.Metadata.Rights = entry.DcRights;
-                if (entry.Series) {
-                    entry.Series.forEach((s) => {
-                        const coll = new OPDSCollection();
-                        coll.Name = s.Name;
-                        coll.Position = s.Position;
-                        const link = new OPDSLink();
-                        link.Href = s.Url;
-                        coll.Links = [];
-                        coll.Links.push(link);
-
-                        if (!p.Metadata.BelongsTo) {
-                            p.Metadata.BelongsTo = new BelongsTo();
-                        }
-                        if (!p.Metadata.BelongsTo.Series) {
-                            p.Metadata.BelongsTo.Series = [];
-                        }
-                        p.Metadata.BelongsTo.Series.push(coll);
-                    });
-                }
-                if (entry.DcPublisher) {
-                    const c = new OPDSContributor();
-                    c.Name = entry.DcPublisher;
-                    if (!p.Metadata.Publisher) {
-                        p.Metadata.Publisher = [];
-                    }
-                    p.Metadata.Publisher.push(c);
-                }
-
-                if (entry.Categories) {
-                    entry.Categories.forEach((cat) => {
-                        const subj = new Subject();
-                        subj.Code = cat.Term;
-                        subj.Name = cat.Label;
-                        subj.Scheme = cat.Scheme;
-                        if (!p.Metadata.Subject) {
-                            p.Metadata.Subject = [];
-                        }
-                        p.Metadata.Subject.push(subj);
-                    });
-                }
-                if (entry.Authors) {
-                    entry.Authors.forEach((aut) => {
-
-                        const cont = new OPDSContributor();
-                        cont.Name = aut.Name;
-                        cont.Identifier = aut.Uri;
-                        if (!p.Metadata.Author) {
-                            p.Metadata.Author = [];
-                        }
-                        p.Metadata.Author.push(cont);
-                    });
-                }
-
-                if (entry.Summary) {
-                    p.Metadata.Description = ((entry.SummaryType === "text/html" || entry.SummaryType === "html") ?
-                        entry.Summary.replace(/ xmlns="[^"]+"/g, "") : // xmlns="http://www.w3.org/2005/Atom"
-                        entry.Summary);
-                }
-
-                if (entry.Content) {
-                    const txt = ((entry.ContentType === "text/html" || entry.ContentType === "html") ?
-                        entry.Content.replace(/ xmlns="[^"]+"/g, "") : // xmlns="http://www.w3.org/2005/Atom"
-                        entry.Content);
-
-                    if (p.Metadata.Description) {
-                        p.Metadata.Description += "\n\n";
-                        p.Metadata.Description += txt;
-                    } else {
-                        p.Metadata.Description = txt;
-                    }
-                }
-
-                if (entry.Links) {
-                    entry.Links.forEach((link) => {
-                        const l = new OPDSLink();
-                        l.Href = link.Href;
-                        l.TypeLink = link.Type;
-                        l.AddRel(link.Rel);
-                        l.Title = link.Title;
-
-                        if (link.OpdsIndirectAcquisitions && link.OpdsIndirectAcquisitions.length) {
-                            if (!l.Properties) {
-                                l.Properties = new OPDSProperties();
-                            }
-
-                            link.OpdsIndirectAcquisitions.forEach((ia) => {
-                                const ind = new OPDSIndirectAcquisition();
-                                ind.TypeAcquisition = ia.OpdsIndirectAcquisitionType;
-                                if (ia.OpdsIndirectAcquisitions && ia.OpdsIndirectAcquisitions.length) {
-                                    ia.OpdsIndirectAcquisitions.forEach((iac) => {
-
-                                        const cia = new OPDSIndirectAcquisition();
-                                        cia.TypeAcquisition = iac.OpdsIndirectAcquisitionType;
-                                        if (!ind.Children) {
-                                            ind.Children = [];
-                                        }
-                                        ind.Children.push(cia);
-                                    });
-                                }
-                                if (!l.Properties.IndirectAcquisitions) {
-                                    l.Properties.IndirectAcquisitions = [];
-                                }
-                                l.Properties.IndirectAcquisitions.push(ind);
-                            });
-                        }
-
-                        if (link.OpdsPrice && link.OpdsPriceCurrencyCode) {
-                            if (!l.Properties) {
-                                l.Properties = new OPDSProperties();
-                            }
-                            l.Properties.Price = new OPDSPrice();
-                            l.Properties.Price.Currency = link.OpdsPriceCurrencyCode;
-                            l.Properties.Price.Value = link.OpdsPrice;
-                        }
-
-                        if (link.HasRel("collection") || link.HasRel("http://opds-spec.org/group")) {
-                            // NOOP
-                        } else if (link.HasRel("http://opds-spec.org/image") ||
-                            link.HasRel("http://opds-spec.org/image/thumbnail")) {
-                            if (!p.Images) {
-                                p.Images = [];
-                            }
-                            p.Images.push(l);
-                        } else {
-                            if (!p.Links) {
-                                p.Links = [];
-                            }
-                            p.Links.push(l);
-                        }
-                    });
-                }
-
+                const p = convertOpds1ToOpds2_EntryToPublication(entry);
                 if (collLink.Href) {
                     opds2feed.AddPublicationInGroup(p, collLink);
                 } else {
@@ -222,41 +267,7 @@ export function convertOpds1ToOpds2(feed: OPDS): OPDSFeed {
                     opds2feed.Publications.push(p);
                 }
             } else {
-                const linkNav = new OPDSLink();
-                linkNav.Title = entry.Title;
-
-                if (entry.Summary) {
-                    const txt = ((entry.SummaryType === "text/html" || entry.SummaryType === "html") ?
-                        entry.Summary.replace(/ xmlns="[^"]+"/g, "") : // xmlns="http://www.w3.org/2005/Atom"
-                        entry.Summary);
-
-                    if (linkNav.Title) {
-                        linkNav.Title += "\n\n";
-                        linkNav.Title += txt;
-                    } else {
-                        linkNav.Title = txt;
-                    }
-                }
-
-                if (entry.Content) {
-                    const txt = ((entry.ContentType === "text/html" || entry.ContentType === "html") ?
-                        entry.Content.replace(/ xmlns="[^"]+"/g, "") : // xmlns="http://www.w3.org/2005/Atom"
-                        entry.Content);
-
-                    if (linkNav.Title) {
-                        linkNav.Title += "\n\n";
-                        linkNav.Title += txt;
-                    } else {
-                        linkNav.Title = txt;
-                    }
-                }
-
-                if (entry.Links && entry.Links[0]) {
-                    linkNav.AddRel(entry.Links[0].Rel);
-                    linkNav.TypeLink = entry.Links[0].Type;
-                    linkNav.Href = entry.Links[0].Href;
-                }
-
+                const linkNav = convertOpds1ToOpds2_EntryToLink(entry);
                 if (collLink.Href) {
                     opds2feed.AddNavigationInGroup(linkNav, collLink);
                 } else {
